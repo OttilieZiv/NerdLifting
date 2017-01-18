be7df3d49e3397c44d4714217106faed4d04526a
OttilieZiv <me@ottilie.co.uk>
2017-01-18 18:02:13 +0000

    Removing bloody pycache. Again. Arg.

--- LiftLikeANerd/LiftLikeANerd/urls.py
+++ LiftLikeANerd/LiftLikeANerd/urls.py
@@ -37,10 +37,8 @@ urlpatterns = [
             'next_page': '/',
         },
         name='logout'),
-    url(r'^search-exercises/$', views.search_exercises),
+    url(r'^admin/', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 ]
